68ea9f04f06c8dd1ce80cf2b4d3fb2efca502df5
Eduar <donsecun@gmail.com>
2023-02-08 20:08:18 +0100

    Add the grid squeleton, created number by javascript finction

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const digitos = document.querySelector('.digitos');
+const operatorGrid = document.querySelector('.operatorGrid');
 
 digitos.style.setProperty('--grid-rows', 3);
 digitos.style.setProperty('--grid-cols', 3);
@@ -7,9 +8,23 @@ digitos.style.setProperty('--grid-cols', 3);
     cell.classList.add('grid-item');
     cell.textContent = c+1;
     digitos.appendChild(cell); 
-    if (c==9){cell.textContent = '*'}
+    if (c==9){cell.textContent = '0'}
     if (c==10){cell.textContent = '.'}
     if (c==11){cell.textContent = 'C'}
   };
 
+  operatorGrid.style.setProperty('--grid-rows', 5);
+  operatorGrid.style.setProperty('--grid-cols', 1);
+    for (c = 0; c < 5; c++) {
+      var cell = document.createElement('div');
+      cell.classList.add('operators');
+      operatorGrid.appendChild(cell); 
+      if (c==0){cell.textContent = '+'}
+      if (c==1){cell.textContent = '-'}
+      if (c==2){cell.textContent = '*'}
+      if (c==3){cell.textContent = '/'}
+      if (c==4){cell.textContent = '='}
+    };
 
+    var data = document.getElementsByClassName('grid-item');
+  
